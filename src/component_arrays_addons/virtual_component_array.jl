7cfa2ad46ea1c2de952b83c241544a6e9368063f
Jolt8 <willemartin@icloud.com>
2026-09-10 13:54:41 -0600

    updated to species_masses instead of species_mass_flows

--- src/component_arrays_addons/virtual_component_array.jl
+++ src/component_arrays_addons/virtual_component_array.jl
@@ -140,7 +140,7 @@ function Base.show(io::IO, A::VirtualFVMArray)
     ax = getfield(A, :axes)
     #println(keys(ax))
     #println(keys(A))
-    #(:mass_fractions, :pressure, :temp, :molar_concentrations, :mw_avg, :species_mass_flows, :net_rates, :Pr, :rho, :heat, :mass_face, :mass)
+    #(:mass_fractions, :pressure, :temp, :molar_concentrations, :mw_avg, :species_masses, :net_rates, :Pr, :rho, :heat, :mass_face, :mass)
     for name in keys(ax)
         println(io, name, " = ", getproperty(A, name)[:])
         val = ax[name]
