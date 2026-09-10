bdeb87b3541a98c7ff358fd481a7959d6d5d9232
Jolt8 <willemartin@icloud.com>
2026-09-10 13:56:50 -0600

    updated to have a more acccurate desciription of why we zero out the caches

--- src/solvers/common_operator_methods/unpack_fvm_state.jl
+++ src/solvers/common_operator_methods/unpack_fvm_state.jl
@@ -1,9 +1,8 @@
 function unpack_fvm_state(du_vec, u_vec, p, t, system)
     du_vec .= 0.0 #zero out all derivatives
 
-    #if you're wondering why we zero out everything when its a SparseConnectivityTracer.Dual{Float64}, it's because for some
-    #reason calling get_tmp() on a SparseConnectivityTracer.Dual{Float64} returns all undef values, so we have to zero it out
-    #ourselves. 
+    #if you're wondering why we zero out everything, it's because for some reason calling get_tmp() returns all undef values, so we have to zero it out ourselves. 
+    #I think this is intended behaviour
     
     if (first(u_vec) + first(p)) isa SparseConnectivityTracer.Dual{Float64}
         du = VirtualFVMArray((du_vec, (get_tmp(system.du_diff_cache, first(u_vec) + first(p)) .= 0.0)), system.du_virtual_axes)
