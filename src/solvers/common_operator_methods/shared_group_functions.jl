a4b1003d1d18cb0067abfda34d0fdc43df869415
Jolt8 <willemartin@icloud.com>
2026-09-17 16:53:36 -0600

    changed order to match default solve order (it looks nice)

--- src/solvers/common_operator_methods/shared_group_functions.jl
+++ src/solvers/common_operator_methods/shared_group_functions.jl
@@ -1,3 +1,18 @@
+function update_region_group!(
+    du, u, p, t,
+    property_update_function!::F, region_cells,
+    cell_volumes, system
+) where {F}
+    for cell_id in region_cells
+        property_update_function!(
+            du, u, p, t, 
+            cell_id,
+            cell_volumes[cell_id],
+            system
+        )
+    end
+end
+
 function solve_connection_group!(
     du, u, p, t,
     flux!::F, cell_neighbors,
@@ -19,35 +34,6 @@ function solve_connection_group!(
     end
 end
 
-function update_region_group!(
-    du, u, p, t,
-    property_update_function!::F, region_cells,
-    cell_volumes, system
-) where {F}
-    for cell_id in region_cells
-        property_update_function!(
-            du, u, p, t, 
-            cell_id,
-            cell_volumes[cell_id],
-            system
-        )
-    end
-end
-
-function solve_region_group!(
-    du, u, p, t,
-    region_function!::G, region_cells,
-    cell_volumes
-) where {G}
-    for cell_id in region_cells
-        region_function!(
-            du, u, p, t, 
-            cell_id,
-            cell_volumes[cell_id]
-        )
-    end
-end
-
 function solve_patch_group!(
     du, u, p, t,
     patch_physics!::F, cell_neighbors,
@@ -65,5 +51,19 @@ function solve_patch_group!(
                 cell_volumes
             )
         end
+    end
+end
+
+function solve_region_group!(
+    du, u, p, t,
+    region_function!::G, region_cells,
+    cell_volumes
+) where {G}
+    for cell_id in region_cells
+        region_function!(
+            du, u, p, t, 
+            cell_id,
+            cell_volumes[cell_id]
+        )
     end
 end
