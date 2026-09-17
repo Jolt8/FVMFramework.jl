64399a892047649a0ce8297786131266fb6ef5fa
Jolt8 <willemartin@icloud.com>
2026-09-17 16:58:56 -0600

    merged

--- src/solvers/common_operator_methods/shared_group_functions.jl
+++ src/solvers/common_operator_methods/shared_group_functions.jl
@@ -20,12 +20,12 @@ function solve_connection_group!(
     cell_neighbor_normals, cell_neighbor_distances, 
     cell_volumes
 ) where {F}
-
     for (idx_a, neighbor_list) in cell_neighbors
-        for (idx_b, face_idx) in neighbor_list
+        for (idx_b, face_idx_a, face_idx_b) in neighbor_list
             flux!(
-                du, u, p, t,
-                idx_a, idx_b, face_idx,
+                du, u, p, t, system,
+                idx_a, face_idx_a,
+                idx_b, face_idx_b,
                 cell_face_areas, cell_face_normals, cell_face_distances,
                 cell_neighbor_normals, cell_neighbor_distances, 
                 cell_volumes
@@ -34,18 +34,50 @@ function solve_connection_group!(
     end
 end
 
+<<<<<<< HEAD
+=======
+function update_region_group!(
+    du, u, p, t, system,
+    property_update_function!::F, region_cells,
+    cell_volumes
+) where {F}
+    for cell_id in region_cells
+        property_update_function!(
+            du, u, p, t, system,
+            cell_id,
+            cell_volumes[cell_id]
+        )
+    end
+end
+
+function solve_region_group!(
+    du, u, p, t, system,
+    region_function!::G, region_cells,
+    cell_volumes
+) where {G}
+    for cell_id in region_cells
+        region_function!(
+            du, u, p, t, system,
+            cell_id,
+            cell_volumes[cell_id]
+        )
+    end
+end
+
+>>>>>>> 1b3ca86ef6ee59c82e3d93370ed54ea3515395d0
 function solve_patch_group!(
-    du, u, p, t,
+    du, u, p, t, system,
     patch_physics!::F, cell_neighbors,
     cell_face_areas, cell_face_normals, cell_face_distances,
     cell_neighbor_normals, cell_neighbor_distances,
     cell_volumes
 ) where {F} 
     for (idx_a, neighbor_list) in cell_neighbors
-        for (idx_b, face_idx) in neighbor_list
+        for (idx_b, face_idx_a, face_idx_b) in neighbor_list
             patch_physics!(
-                du, u, p, t,
-                idx_a, idx_b, face_idx,
+                du, u, p, t, system,
+                idx_a, face_idx_a,
+                idx_b, face_idx_b,
                 cell_face_areas, cell_face_normals, cell_face_distances,
                 cell_neighbor_normals, cell_neighbor_distances,
                 cell_volumes
